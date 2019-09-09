7412c0d780e51d938e8606e5bd5294b4e68a8602
Prevenir-Desarrollo <prevenir@iMac-de-Prevenir-Desarrollo.local>
2019-09-09 10:59:00 -0500

    Nuevos archivos inicio appi

---
+++ src/routes/imgRoutes.js
@@ -0,0 +1,20 @@
+const img = require('../models/imagenes');
+const jwts = require('../models/jwt');
+
+module.exports=function(app)
+{
+//guarda las imagenes en el servido y en la base de datos
+app.post('/images',(req, res)=>{
+console.log(req.files.archivo);
+var imagen = req.files.archivo;
+img.subida(imagen,req);
+//  var img = {
+//  creador: res.locals.user_id
+//  };
+
+});
+
+//devuelve las imagenes
+app.get('/images',(req,res)=>{img.ver((err,data)=>{res.json(data);});});
+app.get('/',jwts.valida,(req, res) => {User.getUsers((err, data) => {res.json(data);});});
+}
